1faeef47476ad3aa4963264aa58159145af153ed
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-04-10 12:48:32 -0300

    Adding deezer and homescreen

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ window.onload = function() {
 openPage = function(page) {
     window.location.href = page;
 }
+/*
+$(window).scroll(function (e) {
+    var $el = $('.deezerElement');
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if ($(this).scrollTop() > 200 && !isPositionFixed) {
+        $el.css({ 'position': 'fixed', 'top': '0px' });
+    }
+    if ($(this).scrollTop() < 200 && isPositionFixed) {
+        $el.css({ 'position': 'static', 'top': '0px' });
+    }
+});*/
