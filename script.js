40f463d6149a4a31961117c7bc10c57c597cffd3
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-06-10 19:03:36 -0300

    Adding Comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-window.onload = function() {
-
+var commentsData = {};
+window.onload = function () {
+    commentsData = loadInputData();
 }
 openPage = function(page) {
     window.location.href = page;
+    $('#commentsAreaId').html(loadCommentsPage(page));
 }
 /*
 $(window).scroll(function (e) {
@@ -15,3 +17,43 @@ $(window).scroll(function (e) {
         $el.css({ 'position': 'static', 'top': '0px' });
     }
 });*/
+
+loadInputData = function(){
+    var fullData = {};
+    return fullData;
+}
+
+loadCommentsPage = function (page) {
+    var fullData = commentsData[page];
+    var htmlValue = '';
+    fullData.forEach(function(item){
+        htmlValue += getCommentHTML(item.Comment, item.Date)
+    }, { htmlValue});
+    return htmlValue;
+}
+
+sendCommentData = function(idType){
+    var elementComments = $('#'+idType);
+    elementComments.append(getCommentHTML());
+}
+
+getCommentHTML = function(){
+    var user = $('#userNameInput').val();
+    var comments = $('#textAreaInput').val();
+    if (user == '' || comments == ''){
+        return '';
+    }
+    var dateToday = new Date();
+    var htmlVal = `<li class="media">
+                <div class="media-body">
+                    <strong class="text-success">${user}</strong>
+                    <span class="text-muted pull-right">
+                        <small class="text-muted">${dateToday.toLocaleDateString()} ${dateToday.toLocaleTimeString()}</small>
+                    </span>
+                    <p>${comments}</p>
+                </div>
+            </li>`;
+    $('#userNameInput').val('');
+    $('#textAreaInput').val('');
+    return htmlVal;
+}
