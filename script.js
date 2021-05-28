396a1cef65d57d8083355300b06bde9bf4eed4f1
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2021-05-28 11:26:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ openPage = function(page) {
     $('#commentsAreaId').html(loadCommentsPage(page));
 }
 /*
+ALLAN VIADO
 $(window).scroll(function (e) {
     var $el = $('.deezerElement');
     var isPositionFixed = ($el.css('position') == 'fixed');
